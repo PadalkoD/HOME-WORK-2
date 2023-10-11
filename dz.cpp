092d1c1ffe68bd161d8078e10489f22ce01bad65
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2023-10-11 16:02:24 +0300

    Add files via upload

---
+++ dz.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+  /*
+    nomer 1 
+  int t;
+    std::cin >> t;
+    std::cout << t / 3600 << " hours " << (t % 3600 / 60) << " minuts " << (t % 3600 % 60) << " seconds" << std::endl;
+    return 0;*/
+   
+
+
+
+   /* nomer 2 
+   float denga;
+    int a,b;
+    cout << "enter a monetary unit" << endl;
+    cin >> denga;
+    a = denga;
+    b = (denga - a) * 100;
+    cout << a << " dollar" << b << " cent ";*/
+
+
+    /* nomer 3
+    int day1,week,day2;
+cout << "quantity day" << endl;
+cin>>day1;
+week = day1 / 7;
+day2 = day1 % 7;
+cout << "week:" << week << " day:" << day2;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

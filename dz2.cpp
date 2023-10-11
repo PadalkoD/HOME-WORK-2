b8f139cea5bb46cac866a5953ce5ca3bca1cfee7
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2023-10-11 20:32:20 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	/* nomer 1
+	int nam1, a1, a2, a3, a4, a5, a6, sum1, sum2;
+	cout « "enter number:";
+	cin » nam1;
+	if (nam1 >=100000 && nam1<1000000) {
+	 a1 = nam1 / 100000;
+	 a2 = nam1 / 10000 % 10;
+	 a3 = nam1 / 1000 % 10;
+	 a4 = nam1 / 100 % 10;
+	 a5 = nam1 / 10 % 10;
+	 a6 = nam1 % 10;
+	 sum1 = a1 + a2 + a3;
+	 sum2 = a4 + a5 + a6;
+	 if (sum1 == sum2)cout « "lucky";
+	 else cout « "unlucky";
+	}
+	else cout « "error";
+
+*/
+	/* nomer 2 
+	int nam1, a1, a2, a3, a4;
+	cout « "enter number:";
+	cin » nam1;
+	a1 = nam1 / 1000 % 10;
+	a2 = nam1 / 100 % 10;
+	a3 = nam1 / 10 % 10;
+	a4 = nam1 % 10;
+	nam1 = (a2 * 1000) + (a1 * 100) + (a4 * 10) + a3;
+	cout « nam1;*/
+
+	/*int max, a1, a2, a3, a4, a5, a6, a7;
+	cout « "enter 7 number:"«endl;
+	cin » a1 » a2 » a3 » a4 » a5 » a6 » a7;
+	max = 0;
+	if (max < a1)max = a1;
+	if (max < a2)max = a2;
+	if (max < a3)max = a3;
+	if (max < a4)max = a4;
+	if (max < a5)max = a5;
+	if (max < a6)max = a6;
+	if (max < a7)max = a7;
+	cout « max;*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

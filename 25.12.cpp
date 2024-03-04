462d4d603d581c16ed33cbcd7f9306346d13c364
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2024-03-04 21:50:19 +0300

    Add files via upload

---
+++ 25.12.cpp
@@ -0,0 +1,30 @@
+﻿
+##include <iostream>
+using namespace std;
+
+#define random(a, b) a+rand()%(b-a+1)
+
+int findindex(int array[]) {
+
+}
+
+void fillArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = random(1, 10);
+    }
+}
+
+void showArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int size = sizeof(array) / sizeof(array[0]);
+    int array[10]{};
+    fillArray(array);
+    showArray(array);
+}

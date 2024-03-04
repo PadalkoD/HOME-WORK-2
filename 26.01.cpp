2742d22f8a1ee73c6b4827f8040dfb458a417929
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2024-03-04 19:15:48 +0300

    Add files via upload

---
+++ 26.01.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int math(int basDegree, int indiDegree) {
+    int answer = basDegree;
+    for (int i = 1; i < indiDegree; i++) {
+        answer *= basDegree;
+    }
+    return answer;
+}
+
+
+int summNumbers(int min, int max) {
+    int summ = 0;
+    for (int i = min; i <= max; i++) {
+        summ += i;
+    }
+    return summ;
+}
+
+int main()
+{
+    //cout << math(3, 3); // number 1
+    cout << summNumbers(1, 5);
+
+
+
+}
+
+
+int main()
+{
+    int array[] = { 3,5,2,1,5 };
+    int size = sizeof(array) / sizeof(array[0]);
+    int array1[2][5] = { {3,4,65,1,5},{4,2,1,762,2} };
+    int sizeSecArr = sizeof(array1[0]) / sizeof(array1[0][0]);
+    int sizeFirArr = sizeof(array1) / sizeof(array1[0]);
+    showArray1(array1, sizeFirArr, sizeSecArr);
+    swapArray2(array1, sizeSecArr);
+    showArray(array1, size3, size1);
+    showArray(array, size); 
+
+}

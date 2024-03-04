3326b10ea7bd0e857539507ad7b84f1fe8d93f3a
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2024-03-04 19:05:21 +0300

    Add files via upload

---
+++ 06.10.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	/*char simb;
+	cout << "Введите символ.";
+	cin >> simb;
+	if (simb >= 65 && simb <= 90)& (simb >= 97 && simb <= 122)& (simb >= 128 && simb <= 175)& (simb >= 224 && simb <= 241)
+	{
+
+	}
+	else if (simb >= 48 && simb <= 57)
+	{
+		cout << "Цифра";
+	}
+	else
+	{
+		cout << "blb";
+	}*/
+
+ №2
+
+	/*short min, number;
+
+	cout << "МТС - 1, ТЕЛЕ2 - 2, МЕГАФОН - 3" << endl;
+	cin >> number;
+	cout << "Напишите длительность в минутах." << endl;
+	cin >> min;
+
+	switch (number)
+	{
+	case 1:
+		cout << min * 3 << "- цена разговора" << endl;
+		break;
+	case 2:
+		cout << min * 5 << "- цена разговора" << endl;
+		break;
+	case 3:
+		cout << min * 4 << "- цена разговора" << endl;
+		break;
+	default: cout << "ERROR";
+	}*/
+
+}
